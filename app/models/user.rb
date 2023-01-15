cc32ebf69f7c62e44bbca00fb2667beccc71f121
dmacinto <dmacinto@gmail.com>
2023-01-15 21:08:23 +0000

    #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+  
+  has_many :comments, foreign_key: "author_id"
+
+  has_many :likes, foreign_key: "user_id"
+
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
+
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
+ 
+
+
+
+  #has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
+  #has_many(:followers, { :through => :received_follow_requests, :source => :sender })
+ # has_many(:liked_photos, { :through => :likes, :source => :photo })
+  #has_many(:feed, { :through => :following, :source => :own_photos })
+  #has_many(:activity, { :through => :following, :source => :liked_photos })
+
+
 end
