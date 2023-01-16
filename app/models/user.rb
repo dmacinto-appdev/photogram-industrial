e9ff7916b7c130f2980bf6941e25c0ad9196d113
dmacinto <dmacinto@gmail.com>
2023-01-16 12:25:35 +0000

    added direct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,17 +28,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  
-  has_many :comments, foreign_key: "author_id"
+  has_many :comments, foreign_key: :author_id
 
-  has_many :likes, foreign_key: "user_id"
+  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: :sender_id
 
-  has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
-
-  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
+  has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: :recipient_id
  
+  has_many :likes, foreign_key: :fan_id
 
+  has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
 
 
   #has_many(:following, { :through => :sent_follow_requests, :source => :recipient })
