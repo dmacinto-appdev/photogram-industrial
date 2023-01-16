e2d26d06515e34d6dbfa4160c50c81b2cfff8208
dmacinto <dmacinto@gmail.com>
2023-01-16 20:25:36 +0000

    random cleanup

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -27,4 +27,8 @@ class FollowRequest < ApplicationRecord
 
   scope :accepted, -> { where(status: "accepted" ) }
   scope :not_accepted, -> { where.not(status: "accepted" ) }
+
+  validates :user_id, uniqueness: { scope: :sender_id, message: "has already sent a follow request" }
+
+  validates :user_id, uniqueness: { scope: :recipient_id, message: "has already received a follow request" }
 end
